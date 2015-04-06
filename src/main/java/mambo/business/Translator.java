2f1e03e817870863ee1375a6c2cf115c499b88bc
Endeios <endeios@gmail.com>
2015-04-06 11:23:07 +0200

    changed the interface signature to be more stream oriented

--- src/main/java/mambo/business/Translator.java
+++ src/main/java/mambo/business/Translator.java
@@ -1,5 +1,14 @@
 package mambo.business;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.springframework.web.multipart.MultipartFile;
+
 public interface Translator {
+
+	ByteArrayOutputStream translate(MultipartFile file) throws IOException,
+			InvalidFormatException;
 
 }
